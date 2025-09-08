32e7468039048d4b256e96e49e6d8b8eaa2cfe32
LIJA30 <jade.li@oocl.com>
2025-09-08 11:08:16 +0800

    add: init person pojo

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,54 @@
 package oo;
 
-public class Person {
+import java.util.Objects;
+
+public abstract class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract String introduce();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
 }
