412fbc5fff82856b6e32f8c42f5c9481f339bc84
LIJA30 <jade.li@oocl.com>
2025-09-08 11:13:32 +0800

    add: add step2 teacher junit test case

--- src/test/java/oo/step2/TeacherTest.java
+++ src/test/java/oo/step2/TeacherTest.java
@@ -1,10 +1,30 @@
 package oo.step2;
 
+import oo.Teacher;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TeacherTest {
 
     // when introduce a teacher, then return a message with name and age
-
+    @Test
+    public void should_return_message_with_name_and_age() {
+        Teacher teacher = new Teacher(1, "Jack", 50);
+        assertEquals(teacher.introduce(), "My name is Jack. I am 50 years old. I am a teacher.");
+    }
     // when equals two same id teacher then return true
-
-    // when equals two different id teacher then return true
+    @Test
+    public void should_return_true_when_equals_two_same_id_teacher() {
+        Teacher teacher1 = new Teacher(1, "Jack", 50);
+        Teacher teacher2 = new Teacher(1, "Jack", 50);
+        assertTrue(teacher1.equals(teacher2));
+    }
+    // when equals two different id teacher then return false
+    @Test
+    public void should_return_false_when_equals_two_different_id_teacher() {
+        Teacher teacher1 = new Teacher(1, "Jack", 50);
+        Teacher teacher2 = new Teacher(2, "Tom", 50);
+        assertFalse(teacher1.equals(teacher2));
+    }
 }
